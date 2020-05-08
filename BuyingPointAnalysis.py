72a72592da0a85949ab50fca6edc159dc0af8912
ForrestLin96 <63614850+ForrestLin96@users.noreply.github.com>
2020-05-08 15:52:45 -0400

    Update BuyingPointAnalysis.py

--- BuyingPointAnalysis.py
+++ BuyingPointAnalysis.py
@@ -6,7 +6,7 @@ import pandas_datareader.data as web
 from pandas import Series, DataFrame
 import matplotlib.pyplot as plt
 import matplotlib
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score,cross_val_predict
 from sklearn import tree
 from sklearn.naive_bayes import GaussianNB
 from sklearn import linear_model, datasets
@@ -14,7 +14,18 @@ from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier as Gxb
 from sklearn.model_selection import TimeSeriesSplit
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import precision_recall_curve
 matplotlib.style.use('ggplot')
+
+def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
+    plt.plot(thresholds, precisions[:-1], "b--", label="Precision")
+    plt.plot(thresholds, recalls[:-1], "g-", label="Recall")
+    plt.xlabel("Threshold")
+    plt.legend(loc="lower left")
+    plt.xlim([0.1, 1])
+    plt.ylim([0.1, 1])
+    plt.title([stock,method_list.columns[index]])
 
 #Define Stochastic Osciliator
 def calculate_k(ticker_df,cycle, M1 ):
@@ -50,23 +61,24 @@ def buyjudge(ticker_df,gain=0.024,cycle=10):
     df.loc[(df['Max']>(1+gain)*ticker_df['Close']),'Good?'] = 1#& (ticker_df['Min']<*ticker_df['Close'])
     return ticker_df
 
-method_list = [{'DecisionTree()':tree.DecisionTreeClassifier(),
-                'GaussianNB()':GaussianNB(),
-                'svm.SVC(C=1)':svm.SVC(),
-                'svm.SVC(C=1.5)':svm.SVC(C=1.5),
-                'svm.SVC(C=2)':svm.SVC(C=2),
-                'svm.SVC(kernel=linear, C=2)':svm.SVC(kernel='linear'),
-                'svm.SVC(kernel=poly, C=2)':svm.SVC(kernel='poly'),
+method_list = [{#'DecisionTree':tree.DecisionTreeClassifier(),
+                'Bayes':GaussianNB(),
+                #'LogisticRegression':LogisticRegression(),
+                'SVC(C=1)':svm.SVC(probability=True),
+                'SVC(C=1.5)':svm.SVC(C=1.5,probability=True),
+                'SVC(C=2)':svm.SVC(C=2,probability=True),
+                'SVC(linear, C=2)':svm.SVC(kernel='linear',probability=True),
+                'SVC(poly, C=2)':svm.SVC(kernel='poly',probability=True),
                 'RandomForest(random_state=10)':RandomForestClassifier(random_state=10), 
-                 'XGBT()':Gxb(),
-                # 'XGBT(Lambda=0.8)':Gxb(reg_lambda=0.8),
-                'XGBT(Lambda=1.2)':Gxb(reg_lambda=1.2),
-                'XGBT(Lambda=1.4)':Gxb(reg_lambda=1.4),
-                'XGBT(gamma=0.2)': Gxb(gamma=0.2),
-                'XGBT(gamma=0.3)': Gxb(gamma=0.3),
-                'XGBT(gamma=0.4)': Gxb(gamma=0.4),
-                'XGBT(gamma=0.5)': Gxb(gamma=0.5),
-                'XGBT(gamma=0.55)': Gxb(gamma=0.55)
+                'XGBT()':Gxb(),
+                'XGBT(λ=0.8)':Gxb(reg_lambda=0.8),
+                'XGBT(λ=1.2)':Gxb(reg_lambda=1.2),
+                'XGBT(λ=1.4)':Gxb(reg_lambda=1.4),
+                'XGBT(γ=0.2)': Gxb(gamma=0.2),
+                'XGBT(γ=0.3)': Gxb(gamma=0.3),
+                'XGBT(γ=0.4)': Gxb(gamma=0.4),
+                'XGBT(γ=0.5)': Gxb(gamma=0.5),
+                'XGBT(γ=0.55)': Gxb(gamma=0.55),
                 }]#
 method_list=pd.DataFrame(method_list)
 ResultTable=DataFrame(columns=['Stock','Method','AvgScores','StdScores'])
@@ -94,15 +106,13 @@ for stock in stocklist:
     df['Intersection'] = 0 #buy indicator
     df.loc[(df['K']>df['D']) & (df['K_prev']<df['D_prev']) & (df['D']<=50)  & (df['D_diff']>0),'Intersection'] = 1# Intersections: K go exceeds D   
     df['# Inter 10-day'] = df['Intersection'].rolling(14).sum()# number of intersections during past 10 days    
-    # df['MA50']= df['Close'].rolling(50) Moving Average of the past 50 days
-    df['Close/MA10']= df['Close']/df['Close'].rolling(10).mean()
+    df['Close/MA10']= df['Close']/df['Close'].rolling(10).mean()# df['MA10']= df['Close'].rolling(10) Moving Average of the past 10 days
     df['Close/MA20']= df['Close']/df['Close'].rolling(20).mean()
     df['Close/MA50']= df['Close']/df['Close'].rolling(50).mean()
     df['Close/MA100']= df['Close']/df['Close'].rolling(100).mean()
     df['Close/MA200']= df['Close']/df['Close'].rolling(200).mean()
     buyjudge(df)
-    #Get rid of rows with NA value
-    df.dropna(axis=0, how='any', inplace=True)
+    df.dropna(axis=0, how='any', inplace=True)#Get rid of rows with NA value
     
     # X=df.loc[df['Intersection']==1]
     # X=X.loc[:,['# Inter 10-day','Intersection','MAVOL10','MAVOL5','SP500_ROC','Close_ROC','rsv','K','D','J',
@@ -127,7 +137,7 @@ for stock in stocklist:
     index=0
     for method in method_list.loc[0,:]:
         clf = method
-        cv=TimeSeriesSplit(n_splits=6)
+        cv=TimeSeriesSplit(n_splits=3)
         scores = cross_val_score(clf,X, y, cv=cv,scoring='precision')
         print(scores[scores>0])
         series={'Stock':stock,'Method':method_list.columns[index],'AvgScores':scores[scores>0].mean(),'StdScores':scores[scores>0].std()}
@@ -141,3 +151,23 @@ for stock in stocklist:
     plt.barh(range(len(num_list)), num_list,tick_label = name_list)
     plt.title(stock)
     plt.show()
+
+# # 返回预测标签
+# print(clf.predict(X[-1:]))
+ 
+# # 返回预测属于某标签的概率
+# print(clf.predict_proba(X[-1:]))
+xtrain=X[:3000]
+ytrain=y[:3000]
+xtest=X[3000:]
+ytest=y[3000:]
+index=0
+for method in method_list.loc[0,:]:
+    nb = method
+    nb.fit(xtrain, ytrain)
+    predicted = nb.predict_proba(xtest)
+    precision, recall, threshold = precision_recall_curve(ytest, predicted[:,1])
+    plot_precision_recall_vs_threshold(precision, recall, threshold)
+    index=index+1
+    plt.show()
+   
