1c33d9cfae11667abe651bf5bcfbe07a7ad57b79
ForrestLin96 <63614850+ForrestLin96@users.noreply.github.com>
2020-05-12 16:00:37 -0400

    Update BuyingPointAnalysis.py

--- BuyingPointAnalysis.py
+++ BuyingPointAnalysis.py
@@ -23,9 +23,9 @@ def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
     plt.plot(thresholds, recalls[:-1], "g-", label="Recall")
     plt.xlabel("Threshold")
     plt.legend(loc="lower left")
-    plt.xlim([0.1, 1])
-    plt.ylim([0.1, 1])
-    plt.title([stock,method_list.columns[index]])
+    plt.xlim([0.4, 0.6])
+    plt.ylim([0, 1])
+    plt.title(stock+'/n'+method_list.columns[index])
 
 #Define Stochastic Osciliator
 def calculate_k(ticker_df,cycle, M1 ):
@@ -54,57 +54,53 @@ def stochastic_oscillator(ticker_df,cycle=12, M1=4, M2= 3):
     ticker_df = calculate_k(ticker_df,cycle,M1)
     ticker_df = calculate_dj(ticker_df, M2)
     return ticker_df
-    #Judgem whether an intersection is a good buying point, if in the following 14 days, the price goes up by at leasr 3%, it is good
+#Evenly separeate all days into good buying points and poor buying points
 def buyjudge(ticker_df,gain=0.024,cycle=10):
     ticker_df['Max'] = ticker_df['Close'].rolling(window = cycle).max().shift(-cycle)
     ticker_df['Good?'] =0
     df.loc[(df['Max']>(1+gain)*ticker_df['Close']),'Good?'] = 1#& (ticker_df['Min']<*ticker_df['Close'])
     return ticker_df
 
-method_list = [{#'DecisionTree':tree.DecisionTreeClassifier(),
+method_name = [{#'DecisionTree':tree.DecisionTreeClassifier(),
                 # 'Bayes':GaussianNB(),
                 #'LogisticRegression':LogisticRegression(),
                 # 'SVC(C=1)':svm.SVC(probability=True),
                 # 'SVC(C=1.5)':svm.SVC(C=1.5,probability=True),
                 # 'SVC(C=2)':svm.SVC(C=2,probability=True),
                 # 'SVC(linear, C=2)':svm.SVC(kernel='linear',probability=True),
-                # 'SVC(poly, C=2)':svm.SVC(kernel='poly',probability=True),
+                 'SVC(poly, C=2)':svm.SVC(kernel='poly',probability=True),
                 # 'RandomForest(random_state=10)':RandomForestClassifier(random_state=10), 
-                # 'XGBT()':Xgb(),
-                # 'XGBT(λ=0.8)':Xgb(reg_lambda=0.8),
-                 'XGBT(λ=1.2)':Xgb(reg_lambda=1.2),
-                # 'XGBT(λ=1.4)':Xgb(reg_lambda=1.4),
-                # 'XGBT(γ=0.2)': Xgb(gamma=0.2),
-                # 'XGBT(γ=0.3)': Xgb(gamma=0.3),
-                # 'XGBT(γ=0.4)': Xgb(gamma=0.4),
-                # 'XGBT(γ=0.5)': Xgb(gamma=0.5),
-                # 'XGBT(γ=0.55)': Xgb(gamma=0.55),
-                }]#
-method_list=pd.DataFrame(method_list)
+                 'XGBT()':Xgb(),
+                 'XGBT(λ=0.8)':Xgb(reg_lambda=0.8),
+                'XGBT(λ=1.2)':Xgb(reg_lambda=1.2),
+                'XGBT(λ=1.4)':Xgb(reg_lambda=1.4),
+                'XGBT(γ=0.2)': Xgb(gamma=0.2),
+                'XGBT(γ=0.3)': Xgb(gamma=0.3),
+                'XGBT(γ=0.4)': Xgb(gamma=0.4),
+                'XGBT(γ=0.5)': Xgb(gamma=0.5),
+                 'XGBT(γ=0.55)': Xgb(gamma=0.55)
+                 }]
+method_list=pd.DataFrame(method_name)
 ResultTable=DataFrame(columns=['Stock','Method','AvgScores','StdScores'])
 
 start = datetime.datetime(2005,1,1)
 end = datetime.datetime(2020,5,31)
 df_SP500 = web.DataReader("^GSPC", 'yahoo', start,end)
 
-#Load ticker data
-#stocklist=['MSFT','AAPL','AMZN','GOOG','FB','JNJ','V']
-           #,'PG','JPM','UNH','MA','INTC','VZ','HD','T','PFE','MRK','PEP']
+#Load ticker data   'MSFT','AAPL','AMZN','GOOG','FB','JNJ','V','PG','JPM','UNH','MA','INTC','VZ','HD','T','PFE','MRK','PEP']
 stocklist=['MSFT']
 for stock in stocklist:
     df=web.DataReader(stock, 'yahoo', start, end).drop(columns=['Adj Close'])
     rawdata=df
     #Some indicators
     df['MAVOL10'] = df['Volume']/df['Volume'].rolling(10).mean()
-    df['MAVOL5'] = df['Volume']/df['Volume'].rolling(5).mean()
-    #Eliminate the influence of the market, 
+    df['MAVOL5'] = df['Volume']/df['Volume'].rolling(5).mean() 
     df['SP500'] = df_SP500['Close']
     df['SP500_ROC'] = 100*df['SP500'].diff(1)/df['SP500'].shift(1)
     df['Close_ROC'] = 100*df['Close'].diff(1)/df['Close'].shift(1)   
     stochastic_oscillator(df)
-    
-    df['Intersection'] = 0 #buy indicator
-    df.loc[(df['K']>df['D']) & (df['K_prev']<df['D_prev']) & (df['D']<=50)  & (df['D_diff']>0),'Intersection'] = 1# Intersections: K go exceeds D   
+    df['Intersection'] = 0
+    df.loc[(df['K']>df['D']) & (df['K_prev']<df['D_prev']) & (df['D']<=80)  & (df['D_diff']>0),'Intersection'] = 1# Intersections: K go exceeds D   
     df['# Inter 10-day'] = df['Intersection'].rolling(14).sum()# number of intersections during past 10 days    
     df['Close/MA10']= df['Close']/df['Close'].rolling(10).mean()# df['MA10']= df['Close'].rolling(10) Moving Average of the past 10 days
     df['Close/MA20']= df['Close']/df['Close'].rolling(20).mean()
@@ -113,54 +109,37 @@ for stock in stocklist:
     df['Close/MA200']= df['Close']/df['Close'].rolling(200).mean()
     buyjudge(df)
     df.dropna(axis=0, how='any', inplace=True)#Get rid of rows with NA value
-    
-    # X=df.loc[df['Intersection']==1]
-    # X=X.loc[:,['# Inter 10-day','Intersection','MAVOL10','MAVOL5','SP500_ROC','Close_ROC','rsv','K','D','J',
-    #             'K_ROC','D_ROC','K_diff','D_diff','J_ROC','J_diff','Close/MA10','Close/MA20','Close/MA50',
-    #             'Close/MA100','Close/MA200',]] 
-    # y=df.loc[df['Intersection']==1]
-    # y=y.loc[:,'Good?']
-    
-    
+    #Retrive X and y 
     X=df.loc[:,['# Inter 10-day','Intersection','MAVOL10','MAVOL5','SP500_ROC','Close_ROC','rsv','K','D','J',
                 'K_ROC','D_ROC','K_diff','D_diff','J_ROC','J_diff','Close/MA10','Close/MA20','Close/MA50',
                 'Close/MA100','Close/MA200',]] 
     y=df.loc[:,'Good?']
-   
-    
-    Market_GoodRatio=sum(df['Good?']==1)/len(df['Good?'])
-    Intersection_GoodRatio=sum(df.loc[df['Intersection']==1,'Good?']==1)/len(df.loc[df['Intersection']==1,'Good?'])
+    # split train and test data
+    xtrain=X[:3000]
+    ytrain=y[:3000]
+    xtest=X[3000:]
+    ytest=y[3000:]
+    Market_GoodRatio=sum(df['Good?']==1)/len(df['Good?'])#Good Buying Point Ratio in market is manully set to nearly 0.5 
     ResultTable=ResultTable.append({'Stock':stock,'Method':'Market Good Buying Ratio','AvgScores':Market_GoodRatio,'StdScores':0},ignore_index=True)
-    ResultTable=ResultTable.append({'Stock':stock,'Method':'Intersection Good Buying Ratio','AvgScores':Intersection_GoodRatio,'StdScores':0},ignore_index=True)
         
     #try different classification methods and compare the accuracy
     index=0
     for method in method_list.loc[0,:]:
         clf = method
-        cv=TimeSeriesSplit(n_splits=3)
-        scores = cross_val_score(clf,X, y, cv=cv,scoring='precision')
+        cv=TimeSeriesSplit(n_splits=4)
+        scores = cross_val_score(clf,xtrain, ytrain, cv=cv,scoring='precision')
         print(scores[scores>0])
         series={'Stock':stock,'Method':method_list.columns[index],'AvgScores':scores[scores>0].mean(),'StdScores':scores[scores>0].std()}
         index=index+1
         ResultTable=ResultTable.append(series,ignore_index=True)
 
-name_list= ['Market Good Buying Ratio','Intersection Good Buying Ratio']
+name_list= ['Market Good Buying Ratio']
 name_list=np.append(name_list,method_list.columns)
 for stock in stocklist:
     num_list= ResultTable.loc[ResultTable['Stock']==stock]['AvgScores']
     plt.barh(range(len(num_list)), num_list,tick_label = name_list)
-    plt.title(stock)
+    plt.title(stock+'\nPrecision Rate')
     plt.show()
-
-# # return the predicted values
-# print(clf.predict(X[-1:]))
- 
-# # return the probability of the returned values
-# print(clf.predict_proba(X[-1:]))
-xtrain=X[:3000]
-ytrain=y[:3000]
-xtest=X[3000:]
-ytest=y[3000:]
 
 index=0
 for method in method_list.loc[0,:]:
@@ -172,27 +151,26 @@ for method in method_list.loc[0,:]:
      plt.show()
      index=index+1
      
-#select best method     
-    
+#select best method        
 #clf = svm.SVC(C=2,probability=True)
-clf = Xgb(reg_lambda=1.2)
-clf.fit(xtrain, ytrain)
-predicted = clf.predict_proba(xtest)    
+clfbuy = Xgb(reg_lambda=1.2)
+clfbuy.fit(xtrain, ytrain)
+predicted = clfbuy.predict_proba(xtest)    
 
 #Visualize the buying point
 dfplot=pd.DataFrame()
 dfplot.loc[:,'Close']=df[3000:]['Close']
 dfplot.loc[:,'GoodProb']=predicted[:,1]
-for threshold in np.arange(0.6,0.72,0.01):
+for threshold in np.arange(0.62,0.73,0.02):
     dfplot['Good']=0
     dfplot.loc[(dfplot['GoodProb']>threshold),'Good'] = dfplot['Close']
     x=dfplot.index
     y1=dfplot['Close']
     y2=dfplot['Good']
-    plt.plot(x, y1,'c')
-    plt.plot(x, y2, 'o', ms=4.5)
+    plt.plot(x, y1,'c',label='Price')
+    plt.plot(x, y2, 'o', ms=4.5, label='Buying Point')
     plt.ylim([10, 200])
-    plt.title(['Threshold=',round(threshold)])
+    plt.title(stock+'\nThreshold='+str(round(threshold,3)))
+    plt.legend(loc='upper left')
     plt.show()
-dfplot.to_csv('plot.csv')
-
+#dfplot.to_csv('plot.csv')
